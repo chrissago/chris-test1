e6845b773dbdc82f1c33376724b7eba980604f0e
Chris Camfield <ccamfield@sagomini.com>
2020-03-04 11:52:39 -0500

    Added colour flipping to button

---
+++ Test Project/Assets/Scripts/ToggleButtonColour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleButtonColour : MonoBehaviour
+{
+    [SerializeField] List<Color> colours;
+    [SerializeField] Button targetButton;
+
+    private int _nextColour = 1;
+
+    private void Start()
+    {
+        SetColour(0);
+    }
+
+    private void SetColour(int index)
+    {
+        if (this.targetButton == null || this.colours.Count <= index)
+        {
+            return;
+        }
+
+        Color c = this.colours[index];
+        ColorBlock block = targetButton.colors;
+        block.normalColor = c;
+        block.highlightedColor = c;
+        targetButton.colors = block;
+    }
+
+    public void Toggle()
+    {
+        SetColour(_nextColour);
+        _nextColour = (_nextColour + 1) % 2;
+
+    }
+}
